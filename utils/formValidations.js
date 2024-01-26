6e92156bdfd3cd39f5cd151fe795239d00661ebe
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-01-26 20:07:20 +0500

    🔥Refactor: change directory location

--- utils/formValidations.js
+++
@@ -1,8 +0,0 @@
-import * as z from 'zod';
-
-export const SignInFormSchema = z.object({
-    username: z.string().min(6),
-    email: z.string().email(),
-    password: z.string()
-        .min(6, 'Password must have than 6 characters'),
-})
