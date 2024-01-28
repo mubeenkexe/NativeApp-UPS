7c080221c7cdb5d848d7d783dc40975b112d9bc7
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-01-28 18:53:40 +0500

    🔥Refactor: Modify code and rename App.js from index.js

---
+++ src/app/App.js
@@ -0,0 +1,8 @@
+import AppNavigator from "./appNavigator";
+import { registerRootComponent } from "expo";
+
+const App = () => {
+    return <AppNavigator />;
+};
+
+export default registerRootComponent(App);
