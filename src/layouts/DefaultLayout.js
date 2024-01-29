e98bdf765174f0ae3c4ccf5a7d7cce3dd1bc0367
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-01-29 22:32:29 +0500

    🔥Refactor: Delete useless files

--- src/layouts/DefaultLayout.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-const DefaultLayout = ({ children }) => {
-    return (
-        <View className="flex-1 p-[16px] justify-center items-center">{children}</View>
-    )
-}
-
-export default DefaultLayout;
