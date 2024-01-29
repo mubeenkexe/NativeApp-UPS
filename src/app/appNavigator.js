404655ad5210612d869bc4f302964f8f816f2098
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-01-29 22:33:44 +0500

    ✨ Feat: I did something which doesnt even work

--- src/app/appNavigator.js
+++ src/app/appNavigator.js
@@ -1,13 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { routes } from "../constant/routes";
+import useLogin from "../hooks/useLogin";
 
 const AppNavigator = () => {
     const Stack = createStackNavigator();
+    
+    const {navigation} = useLogin();
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="SignIn">
+        <NavigationContainer ref={navigation} >
+            <Stack.Navigator initialRouteName="SignIn" screenOptions={{headerShown: false}}>
                 {Object.entries(routes).map(([name, { screen, navigationOption }]) => (
                     <Stack.Screen
                         key={name}
