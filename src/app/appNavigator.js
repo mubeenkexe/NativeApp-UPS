dacab299d1c41eda2f45999f3bb72b314005d536
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-01-30 23:22:04 +0500

    🚨Test: Testing solutions for the bug

--- src/app/appNavigator.js
+++ src/app/appNavigator.js
@@ -1,15 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { routes } from "../constant/routes";
-import useLogin from "../hooks/useLogin";
+
 
 const AppNavigator = () => {
     const Stack = createStackNavigator();
-    
-    const {navigation} = useLogin();
 
     return (
-        <NavigationContainer ref={navigation} >
+        <NavigationContainer>
             <Stack.Navigator initialRouteName="SignIn" screenOptions={{headerShown: false}}>
                 {Object.entries(routes).map(([name, { screen, navigationOption }]) => (
                     <Stack.Screen
