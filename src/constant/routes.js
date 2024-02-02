e7aafb7342bbeb77fe175ca7aaed516e67d680b4
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-02 23:13:27 +0500

    ✨ Feat: Add route for ForgotPassScreen.js

--- src/constant/routes.js
+++ src/constant/routes.js
@@ -1,3 +1,4 @@
+import ForgotPassScreen from "../pages/ForgotPassScreen";
 import HomeScreen from "../pages/HomeScreen";
 import SignInScreen from "../pages/SignInScreen";
 import SignUpScreen from "../pages/SignUpScreen";
@@ -19,6 +20,12 @@ export const routes = {
         screen: HomeScreen,
         navigationOption: {
             title: "Home",
+        },
+    },
+    ForgotPass: {
+        screen: ForgotPassScreen,
+        navigationOption: {
+            title: "ForgotPass",
         },
     },
 };
