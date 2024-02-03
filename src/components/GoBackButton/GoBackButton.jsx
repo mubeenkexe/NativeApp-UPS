4f52b78115c946bc88f616d934fcb81b617067b6
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-03 15:27:05 +0500

    ✨ Feat: Create Go Back Button component for better reusibilty

---
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -0,0 +1,15 @@
+import { TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const GoBackButton = ({ handleClick }) => {
+    return (
+        <TouchableOpacity
+            className="border-gray-600 border w-14 h-10 rounded-full items-center justify-center mt-5"
+            onPress={handleClick}
+        >
+            <Icon name="arrow-left" color="white" size={20} />
+        </TouchableOpacity>
+    );
+};
+
+export default GoBackButton;
