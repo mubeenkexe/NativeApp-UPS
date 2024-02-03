c884bfa83249d2825594eedcfc55822ba762dd8e
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-03 16:48:41 +0500

    🔨 Refactor Code: Update component and add option for icon

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,22 +1,27 @@
-import { LinearGradient } from "expo-linear-gradient"
-import { Text, TouchableOpacity } from "react-native"
+import { LinearGradient } from "expo-linear-gradient";
+import { View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const CustomButton = ({ title, BtnColors, handleClick }) => {
+const CustomButton = ({ title, BtnColors, handleClick, iconName }) => {
     return (
-        <TouchableOpacity
-            activeOpacity={0.5}
-            onPress={handleClick}
-        >
+        <TouchableOpacity activeOpacity={0.5} onPress={handleClick}>
             <LinearGradient
                 colors={[...BtnColors]}
-                className="p-4 rounded-full"
+                className="py-4 rounded-full flex-row items-center relative"
             >
-                <Text className="text-white font-bold text-center text-base">
+                {iconName && (
+                    <View className="left-10 absolute">
+                        <Icon name={iconName} color="white" size={20} />
+                    </View>
+                )}
+
+                <Text className="text-white font-bold text-center w-full text-base">
                     {title}
                 </Text>
             </LinearGradient>
         </TouchableOpacity>
-    )
-}
+    );
+};
 
 export default CustomButton;
