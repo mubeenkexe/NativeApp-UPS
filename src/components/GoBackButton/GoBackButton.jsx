1d1846ab395fb58d1a04193ac3441a8e931d1820
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-04 15:09:50 +0500

    🖌️ Style: Add margin bottom in the Parent View component.
    3:09 PM - 2/4/2024

--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -4,7 +4,7 @@ import Icon from "react-native-vector-icons/FontAwesome";
 const GoBackButton = ({ handleClick }) => {
     return (
         <TouchableOpacity
-            className="border-gray-600 border w-14 h-10 rounded-full items-center justify-center mt-5"
+            className="border-gray-600 border w-14 h-10 rounded-full items-center justify-center mt-5 mb-5"
             onPress={handleClick}
         >
             <Icon name="arrow-left" color="white" size={20} />
