e6400bfc58385515e9b65fc8716940842c1d9f12
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-06 00:29:21 +0500

    🚚 Move/rename file(s): Change component file names to page.jsx

--- src/components/CustomButton/page.jsx
+++ src/components/CustomButton/page.jsx
@@ -3,9 +3,9 @@ import { View } from "react-native";
 import { Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-const CustomButton = ({ title, BtnColors, handleClick, iconName }) => {
+const CustomButton = ({ title, BtnColors, handleClick, iconName, isDisbaled }) => {
     return (
-        <TouchableOpacity activeOpacity={0.5} onPress={handleClick}>
+        <TouchableOpacity disabled={isDisbaled}  activeOpacity={0.5} onPress={handleClick}>
             <LinearGradient
                 colors={[...BtnColors]}
                 className="py-4 rounded-full flex-row items-center relative"
