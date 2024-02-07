f3e568187dd04849747da2ad3b5717dd2cf8dbfc
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-07 23:17:07 +0500

    🚧 Work in progress: Create a DropDown component for testing

--- src/components/DropDown/page.jsx
+++ src/components/DropDown/page.jsx
@@ -1,16 +1,7 @@
-import { Picker } from "@react-native-picker/picker";
-const DropDown = ({ selectedValue, options, onValueChange }) => {
+const DropDown = () => {
+
     return (
-        <Picker selectedValue={selectedValue} onValueChange={onValueChange} mode="dropdown" className="bg-white">
-            {options.map((option, index) => (
-                <Picker.Item
-                    key={index}
-                    label={option.label}
-                    value={option.value}
-                    color="white"
-                />
-            ))}
-        </Picker>
+        <></>
     );
 };
 
