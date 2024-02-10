0558be6a4a0b978247281d20601e081dacb7cee2
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-10 23:18:33 +0500

    🔥 Remove Code/file(s): Remove file for old dropdown component

--- src/components/DropDown/page.jsx
+++ src/components/DropDown/page.jsx
@@ -1,7 +1,16 @@
-const DropDown = () => {
+import { Picker } from "@react-native-picker/picker";
 
+const DropDown = ({ options, selectedValue, onValueChange }) => {
     return (
-        <></>
+        <Picker selectedValue={selectedValue} onValueChange={onValueChange}>
+            {options.map((option, index) => (
+                <Picker.Item
+                    key={index}
+                    label={option.label}
+                    value={option.value}
+                />
+            ))}
+        </Picker>
     );
 };
 
