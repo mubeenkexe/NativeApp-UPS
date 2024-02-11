c7be9b968c5fe836f4c8e76c0c76e90da8d4b9fa
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2024-02-11 23:06:45 +0500

    🎨 Style: Make some style changes which would be temporary at the moment because I will be adding better UI letter.

--- src/components/CustomDropDown/page.jsx
+++ src/components/CustomDropDown/page.jsx
@@ -2,14 +2,13 @@ import { View, Text, TouchableOpacity, Animated } from "react-native";
 import { useState, useEffect, useRef } from "react";
 
 const CustomDropDown = ({ options, onSelect }) => {
-
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState(null);
     const translateY = useRef(new Animated.Value(0)).current;
 
     useEffect(() => {
         Animated.timing(translateY, {
-            toValue: isDropdownOpen ? 0 : -1000,
+            toValue: isDropdownOpen ? 200 : 1000,
             duration: 300,
             useNativeDriver: true,
         }).start();
@@ -22,26 +21,29 @@ const CustomDropDown = ({ options, onSelect }) => {
     };
 
     return (
-        <View className="relative">
+        <View className="relative w-full">
             <TouchableOpacity
                 onPress={() => setIsDropdownOpen(!isDropdownOpen)}
+                className="p-3 rounded-3xl bg-slate-900"
             >
-                <Text className="text-white font-bold text-2xl">
+                <Text className="text-white font-bold text-base text-center">
                     {selectedOption ? selectedOption.label : "Select an option"}
                 </Text>
             </TouchableOpacity>
 
             <Animated.View
-                className="absolute bg-white w-full rounded-3xl max-h-200 overflow-hidden "
+                className="fixed w-full rounded-3xl overflow-hidden h-screen bg-slate-900"
                 style={{ transform: [{ translateY }] }}
             >
                 {options.map((option) => (
                     <TouchableOpacity
                         key={option.value}
                         onPress={() => handleOptionSelect(option)}
-                        className="p-5 border-b border-gray-300 bg-slate-900"
+                        className="p-5 border-b border-gray-600 bg-slate-900 "
                     >
-                        <Text className="text-white font-bold text-2xl">{option.label}</Text>
+                        <Text className="text-white font-bold text-base">
+                            {option.label}
+                        </Text>
                     </TouchableOpacity>
                 ))}
             </Animated.View>
